bb0b56aa54981816bff4d6f630311036d9542577
Jeremmy <hizkiajeremmy@gmail.com>
2023-11-14 12:01:21 +0700

    halo ija muflih

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className=" bg-orange-500 min-h-screen w-screen text-pink-900">
-            Hello World!
+        <div className=" bg-orange-500 min-h-screen w-screen text-pink-900 font-poppins font-bold text-center text-5xl">
+            Halo Ija Muflih
         </div>
     )
 }
