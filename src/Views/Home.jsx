3f52ad83884ab6d3244f3c994d49aaf915b77098
Jeremmy <hizkiajeremmy@gmail.com>
2023-11-14 11:32:28 +0700

    making dist dir using build command

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div className=" bg-orange-500 min-h-screen w-screen text-pink-900">
-            Hello World
+            Hello World!!!
         </div>
     )
 }
